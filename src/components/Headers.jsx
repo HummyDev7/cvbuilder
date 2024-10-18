4407ec51417acd6ec3e3c0ada7f3b7a1e9c75a12
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-10-18 11:56:26 +0800

    Add header file for the website

---
+++ src/components/Headers.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (  
+    <div className="sub__header container">
+      <div className="left__content">
+        <h1 className="logo">
+          C<span className="span__text">VB</span>uildMe
+        </h1>
+      </div>
+    </div>
+  );
+}
