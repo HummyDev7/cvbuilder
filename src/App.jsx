6124670dfbb536b0cdd73645d908bd9712cda8b9
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-10-18 12:00:52 +0800

    Add header component with

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Headers'
+
+export default function App() {
+  return (
+    <main className="main__container">
+      <header className="header__container">
+        <Header />
+      </header>
+    </main>
+  )
+}
